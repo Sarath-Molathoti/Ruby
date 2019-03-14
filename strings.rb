044f03c15b18532e4f22bf3e6d82bd8360b03a17
Sarath-Molathoti <sarathmolathoti@gmail.com>
2019-03-14 15:13:36 +0530

    strings and methods

---
+++ strings.rb
@@ -0,0 +1,11 @@
+#strings
+my_name = "Sarath Molathoti"
+puts ("String    : " + my_name)
+puts ("upcase    : " + my_name.upcase())
+puts ("downcase  : " + my_name.downcase())
+puts ("strip     : " + my_name.strip())
+puts my_name.length()
+puts my_name.include? "Molathoti"
+puts my_name[0]
+puts my_name[0,4]
+puts my_name.index("a")
