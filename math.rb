53d4090fca57309a3787e58cb3c4c9e250e34449
Sarath-Molathoti <sarathmolathoti@gmail.com>
2019-03-14 15:30:59 +0530

    math&numbers

---
+++ math.rb
@@ -0,0 +1,18 @@
+puts 4 + 2
+puts 4 - 2
+puts 4 / 2
+puts 4 % 2
+puts 4 * 2
+puts 4 ** 2
+puts -5.3333
+num = 656 
+puts ("My roll no : " + num.to_s)
+num1 = 24.9
+num2 = 45.2
+num3 = -45
+puts num3.abs()
+puts num2.ceil()
+puts num2.floor()
+puts num1.floor()
+puts Math.sqrt(36)
+puts Math.log(2)
